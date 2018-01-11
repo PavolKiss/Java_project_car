40336858b0afd62e203b0e2a03725df5513eff05
Pavol Kiss <pavol.kiss@akademiasovy.com>
2018-01-11 09:37:40 +0100

    added functions

--- src/sk/akademiasovy/java/Main.java
+++ src/sk/akademiasovy/java/Main.java
@@ -1,16 +1,100 @@
 package sk.akademiasovy.java;
 
+import java.util.Random;
+
+@SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        car car1;
-        car car2;
-        car1=new car();
-        car2=new car("Skoda 120L",6.5f);
+        Car car1;
+        Car car2;
+        Car car3;
+        Car car4;
+        car1 = new Car();
+        car2 = new Car("Skoda 120L", 6.5f, "Presov","Gasoline");
+        car3 = new Car("Seat Ibiza", 7.1f, "Bratislava","Diesel");
+        car4=new Car("Chevrolet Camaro",15.5f,"Kosice","Gasoline");
 
-        //znacka auta
         car1.setBrand("Audi A8");
-        car1.setGetConsumption(9.9f);
+        car1.setGasConsumption(9.9f);
 
         car1.printInfo();
+        System.out.println();
+        car2.printInfo();
+
+        car2.startEngine();
+        car2.increaseSpeed(80);
+        System.out.println();
+        car2.printInfo();
+        System.out.println();
+        System.out.println("Cost: KE---)Praha:" + car2.getPrice(1600, 1.33f));
+        car1.startEngine();
+        car1.increaseSpeed(90);
+        car1.turnOnFrontWiper();
+        car1.printInfo();
+
+        System.out.println();
+
+        if (car2.getSpeed() > car1.getSpeed())
+            System.out.println("Skoda 120L is faster");
+        else if (car1.getSpeed() > car2.getSpeed())
+            System.out.println("Audi A8 is faster");
+        else if (car1.getSpeed() == car2.getSpeed())
+            System.out.println("Same speed");
+        car3.printInfo();
+
+        System.out.println();
+
+
+
+        Car[] taxi;
+        taxi= new Car[10];
+        for(int i=0;i<10;i++)
+        {
+            taxi[i]=new Car("Kia Ceed",6.1f,"Kosice");
+        }
+
+
+        for (Car car:taxi) {
+            car.startEngine();
+            Random random = new Random();
+            car.increaseSpeed(random.nextInt(31) + 40);
+
+            car.printInfo();
+            System.out.println();
+        }
+
+        int maxSpeed=0;
+        for(Car car:taxi)
+            if(car.getSpeed()>maxSpeed)
+                maxSpeed=car.getSpeed();
+        System.out.println("Maximum speed is: "+maxSpeed);
+        for(Car car:taxi)
+            if(car.getSpeed()==maxSpeed)
+                car.printInfo();
+
+
+        int average;
+        int count=0;
+        int arrayLength;
+
+        arrayLength=taxi.length;
+        System.out.println();
+        for(Car car:taxi)
+        {
+            count=count+car.getSpeed();
+        }
+        System.out.println("Average of speed is: "+(double)count/arrayLength);
+
+        int r= new Random().nextInt(arrayLength);
+        car4.printInfo();
+
+
+        System.out.println();
+
+
+
+
+
+
     }
 }
--- src/sk/akademiasovy/java/car.java
+++ src/sk/akademiasovy/java/car.java
@@ -1,52 +1,194 @@
 package sk.akademiasovy.java;
 
-public class car {
+import java.util.Random;
+
+class Car {
     private String brand;
     private int speed;
     private boolean engine;
-    private float getConsumption;
+    private float gasConsumption;
+    private boolean frontWiper;
+    private boolean backWiper;
+    private String plate;
+    private String gas;
+    private String color;
 
-    //konstruktor
-    public car()
+
+
+    //constructor
+
+    public Car()
     {
         speed=0;
         engine=false;
-        getConsumption=7.5f;
+        gasConsumption=7.5f;
         brand=null;
-    }
+        frontWiper=false;
+        backWiper=false;
 
-    public car(String brand, float consuption){
+
+
+
+
+    }
+    public  Car(String brand, float consumption)
+    {
         speed=0;
         engine=false;
-        getConsumption=consuption;
+        gasConsumption=consumption;
         this.brand=brand;
+
+
+    }
+
+    public  Car(String brand, float consumption,String city)
+    {
+        speed=0;
+        engine=false;
+        gasConsumption=consumption;
+        this.brand=brand;
+        generatePlate(city);
+
+
     }
 
 
-    public void setBrand(String value) {
+    public  Car(String brand, float consumption,String city,String gas)
+    {
+        speed=0;
+        engine=false;
+        gasConsumption=consumption;
+        this.brand=brand;
+        generatePlate(city);
+        this.gas=gas;
+
+
+    }
+
+
+
+
+    private void generatePlate(String city)
+    {
+
+        switch(city.toLowerCase())
+        {
+            case"trencin" : plate="TN-"; break;
+            case"presov" : plate="PO-" ;break;
+            case"bratislava" : plate="BA-"; break;
+            case"nitra" : plate="NR-"; break;
+            case"senec" : plate="SC-"; break;
+            default: plate="KE-";
+
+        }
+
+        Random random = new Random();
+        int i;
+        for (i=0;i<3;i++)
+        {
+            int randomNumber=random.nextInt(10);
+            plate=plate+randomNumber;
+        }
+
+        for (i=0;i<2;i++)
+        {
+            int randomNumber=random.nextInt(26)+'A';
+            plate=plate+(char)randomNumber;
+        }
+
+
+    }
+
+
+
+
+
+
+    public void setBrand(String value)
+    {
         brand=value;
     }
 
-    public void setGetConsumption(float getConsumption)
+    public void setGasConsumption(float gasConsumption)
     {
-        this.getConsumption=getConsumption;
+        this.gasConsumption=gasConsumption;
+
+    }
+    public void printInfo()
+    {
+        System.out.println("Brand:"+brand);
+        System.out.println("GasConsumption:"+gasConsumption);
+        System.out.println("Speed:"+speed);
+        System.out.println("Engine:"+engine);
+        System.out.println("FronWiper:"+frontWiper);
+        System.out.println("Plate:"+plate);
+        System.out.println("Gas:"+gas);
+
+
     }
 
-    public void printInfo(){
-        System.out.println("Brand "+brand);
-        System.out.println("Gas "+getConsumption);
-        System.out.println("Speed "+speed);
-    }
-
-    public void startEngine(){
+    public void startEngine()
+    {
         engine=true;
     }
 
-    public void stopEngine(){
+    public void stoptEngine()
+    {
         engine=false;
     }
 
-    public void increaseSpeed(){
-        speed=speed+10;
+    public void  increaseSpeed()
+    {
+        if(engine)
+            speed=speed+10;
+
     }
+
+    public void  increaseSpeed(int value)
+    {
+        if(engine&&value>0)
+            speed+=value;
+
+    }
+
+    public void decreaseSpeed()
+    {
+        speed-=10;
+        if(speed<0)
+            speed=0;
+
+    }
+    public void stop()
+    {
+
+        speed=0;
+    }
+
+    public float getPrice(int distance, float costPerLiter)
+    {
+        float price;
+        price=distance*gasConsumption*costPerLiter/100;
+        return price;
+
+    }
+
+    public int getSpeed()
+    {
+
+        return speed;
+    }
+
+
+    public void turnOnFrontWiper()
+    {
+        frontWiper=true;
+    }
+
+    public void turnOffFrontWiper()
+    {
+        frontWiper=false;
+    }
+
+
+
 }
